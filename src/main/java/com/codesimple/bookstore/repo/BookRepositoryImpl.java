7cb4197eef5c6eb76ba2502d51c1c5f3364d7b03
Vithushayini <139032072+Vithushayini@users.noreply.github.com>
2024-02-04 14:39:19 +0530

    select only particular columns using QueryDSL

--- src/main/java/com/codesimple/bookstore/repo/BookRepositoryImpl.java
+++ src/main/java/com/codesimple/bookstore/repo/BookRepositoryImpl.java
@@ -2,10 +2,12 @@ package com.codesimple.bookstore.repo;
 
 import com.codesimple.bookstore.entity.Book;
 import com.codesimple.bookstore.entity.QBook;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQuery;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookRepositoryImpl implements BookRepositoryCustom{
@@ -23,9 +25,34 @@ public class BookRepositoryImpl implements BookRepositoryCustom{
 
 
         //return
-        return jpaQuery
+//        return jpaQuery
+//                .from(qBook)
+//                .where(qBook.yearOfPublication.eq(year))
+//                .fetch();
+//    }
+
+        //select id,bookType from book where year_of_publication = year;
+
+        //return
+        List<Tuple> tuples=jpaQuery
+                .select(qBook.id,qBook.bookType)
                 .from(qBook)
                 .where(qBook.yearOfPublication.eq(year))
                 .fetch();
+
+        List<Book> books=new ArrayList<>();
+
+        for (Tuple eachTuple:tuples){
+//            System.out.println(eachTuple.get(qBook.id));
+//            System.out.println(eachTuple.get(qBook.bookType));
+            Book book=new Book();
+            book.setId(eachTuple.get(qBook.id));
+            book.setBookType(eachTuple.get(qBook.bookType));
+
+            books.add(book);
+        }
+
+        return books;
     }
+
 }
